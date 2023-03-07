206020db26defbe8526c7a9125e8429a0cccffff
Dustin Freeman <dustin.freeman@gmail.com>
2023-03-06 19:26:21 -0500

    Squashed commit of the following:
    commit 9e37f054edbd5eb78c970acd0b9e1ebd8f614799
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Mon Mar 6 19:22:37 2023 -0500
    
        linting, formatting
    
    commit ca2bce7aaa9997baae61567c6fb169d8387bd224
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Mon Mar 6 19:05:27 2023 -0500
    
        parcel out
    
    commit 99ae23d157a1fbc858df3bb76e76a16f89694fbc
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Mon Mar 6 13:56:00 2023 -0500
    
        button response test
    
    commit 2122980a31e49da0b3e3db982b8b07fa46ae27e3
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Mon Mar 6 13:42:27 2023 -0500
    
        autozip on macos
    
    commit 27fce1e8206ff7f6d6576fed1fbc6d8dd3efdef9
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Sun Mar 5 22:00:32 2023 -0500
    
        the perfect canvas
    
    commit 1974e760fc0eb6a5799dc6d2521c675764d5c9cb
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Sun Mar 5 21:18:59 2023 -0500
    
        enhance viewports
    
    commit 196fef3c821dfbdcc198c6950e34b103bf267900
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Sun Mar 5 21:07:44 2023 -0500
    
        canvas size testing
    
    commit e9d6d676774f6b4b3173476f8fbfa55435924fb0
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Sun Mar 5 20:17:18 2023 -0500
    
        style mockup
    
    commit 527734e0c79005d9e0a2959a6ccd6129ee0dcf7c
    Author: Dustin Freeman <dustin.freeman@gmail.com>
    Date:   Sun Mar 5 18:33:37 2023 -0500
    
        styled-init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import styled, { CSSObject } from 'styled-components';
+import * as ResponsiveApp from './ResponsiveApp';
+
+const Title = styled.h3({ top: '20%', color: '#66bb66' });
+
+const BaseButton: CSSObject = {
+  fontSize: '1em',
+  textAlign: 'center',
+  textDecorationLine: 'underline',
+  color: 'palevioletred',
+  fontFamily: 'monospace',
+  position: 'relative',
+  borderRadius: '0px'
+};
+
+const DelveButton = styled.button({
+  ...BaseButton,
+  position: 'absolute',
+  top: '120px',
+  margin: '0 -30px  0 -30px'
+});
+
+const LeftButton = styled.button({
+  ...BaseButton,
+  position: 'absolute',
+  top: '50px',
+  left: '5px'
+});
+const RightButton = styled.button({
+  ...BaseButton,
+  position: 'absolute',
+  top: '80px',
+  right: '5px'
+});
 
 function App() {
+  const canvasRef = React.createRef<HTMLCanvasElement>();
+
+  //canvas drawing
+  const bgs = ['#f006', '#0f06', '#00f6', '#0006'];
+  const [canvasBGIndex, setCanvasBGIndex] = useState(0);
+
+  useEffect(() => {
+    const canvas = canvasRef?.current;
+    if (!canvas) {
+      return;
+    }
+    const ctx = canvas.getContext('2d'); //, { alpha: false });
+    if (!ctx) {
+      return;
+    }
+
+    ctx.fillStyle = bgs[canvasBGIndex];
+    ctx.fillRect(0, 0, ResponsiveApp.width, ResponsiveApp.height);
+
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'red';
+    const sqWidth = 5;
+    //left delve!?
+    ctx.strokeRect(5 - sqWidth / 2, 50 - sqWidth / 2, sqWidth, sqWidth);
+    //right pass blunt
+    ctx.strokeRect(
+      ResponsiveApp.width - 5 - sqWidth / 2,
+      80 - sqWidth / 2,
+      sqWidth,
+      sqWidth
+    );
+    //delve!
+    ctx.strokeRect(
+      ResponsiveApp.width / 2,
+      120 - sqWidth / 2,
+      sqWidth,
+      sqWidth
+    );
+
+    console.log('canvas draw one time', canvas.width, canvas.height);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div>Dustin wrote this.</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ResponsiveApp.RootDiv canvasRef={canvasRef}>
+      <ResponsiveApp.Overlay>
+        <Title>Blunt Quaternion</Title>
+        <LeftButton>Delve!?</LeftButton>
+        <RightButton
+          onClick={() => {
+            setCanvasBGIndex((canvasBGIndex + 1) % bgs.length);
+          }}>
+          Pass Blunt
+        </RightButton>
+        <DelveButton>Delve!</DelveButton>
+      </ResponsiveApp.Overlay>
+    </ResponsiveApp.RootDiv>
   );
 }
 
---
+++ src/ResponsiveApp.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+import styled, { CSSObject } from 'styled-components';
+import './App.css';
+
+export const width = 300;
+export const height = 225;
+
+const Root = styled.div({
+  display: 'block',
+  textAlign: 'center',
+  verticalAlign: 'middle',
+  backgroundColor: '#335cae',
+  width: width + 'px',
+  height: height + 'px',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transformOrigin: '0 0'
+});
+
+export const ParentFill: CSSObject = {
+  width: '100%',
+  height: '100%',
+  positive: 'relative'
+};
+
+const Canvas = styled.canvas({
+  ...ParentFill
+});
+
+export const Overlay = styled.div({
+  ...ParentFill,
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: width + 'px',
+  height: height + 'px',
+  backgroundColor: '#ff000009'
+});
+
+export function RootDiv(props: {
+  children: React.ReactNode;
+  canvasRef: React.RefObject<HTMLCanvasElement>;
+}) {
+  const appContainerRef = React.createRef<HTMLDivElement>();
+
+  //window resizing
+  const handleWindowSizeChange = () => {
+    const appContainer = appContainerRef.current;
+    if (!appContainer) {
+      return;
+    }
+    const scale = Math.min(
+      window.innerWidth / width,
+      window.innerHeight / height
+    );
+    appContainer.style.transform = 'scale(' + scale + ') translate(-50%, -50%)';
+  };
+  useEffect(() => {
+    handleWindowSizeChange();
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  });
+  return (
+    <Root ref={appContainerRef}>
+      <Canvas ref={props.canvasRef} width={width} height={height} />
+      {props.children}
+    </Root>
+  );
+}
